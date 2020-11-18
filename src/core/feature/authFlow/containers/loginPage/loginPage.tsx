6d28077641bcb8976800d3bfee293f38371c27c6
Vladislav Dontsov <dontsov.vladislav@gmail.com>
2020-11-18 18:00:15 +0200

    did little styling for login modal

--- src/core/feature/authFlow/containers/loginPage/loginPage.tsx
+++ src/core/feature/authFlow/containers/loginPage/loginPage.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import styles from './loginPage.module.scss';
 
 export const LoginPage = (): JSX.Element => {
+  const [values, setValues] = React.useState({
+    login: '',
+    password: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <Container className={styles.wrapper}>
+      <Box className={styles.loginBox}>
+        <Typography className={styles.tittle} variant="subtitle1">
+          Login your account
+        </Typography>
+        <form noValidate autoComplete="off">
+          <Box className={styles.inputGroup}>
+            <FormControl className={styles.input}>
+              <InputLabel htmlFor="loginInput">login</InputLabel>
+              <Input onChange={handleChange} id="loginInput" name="login" />
+            </FormControl>
+            <FormControl className={styles.input}>
+              <InputLabel htmlFor="passwordInput">password</InputLabel>
+              <Input onChange={handleChange} id="passwordInput" className={styles.input} name="password" />
+            </FormControl>
+          </Box>
+          <Button className={styles.loginButton} color="primary" variant="contained">
+            Sign in
+          </Button>
+        </form>
+      </Box>
+    </Container>
   );
 };
